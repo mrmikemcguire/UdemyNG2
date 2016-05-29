57b8fd41dc9f5b4f457e16ba2a0bf532119ddc25
Mike McGuire <MoonshotMike@gmail.com>
2016-05-29 16:45:56 -0600

    cleaned up but puzzle code is not working

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,18 +1,14 @@
 import {Component} from 'angular2/core';
-import {SecondComponent} from './second.component';
-import {ThirdComponent} from './third.component';
-
+import {PuzzleComponent} from "./puzzle/puzzle.component";
 
 @Component({
-    selector: 'my-app',
+    selector: 'app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello My World!</p>
-        <second-component></second-component>
-        <third-component></third-component>
-    `,
-    directives: [SecondComponent, ThirdComponent]
+        <my-puzzle></my-puzzle>
+        `,
+    directives: [PuzzleComponent]
 })
-export class AppComponent {
 
+export class AppComponent {
+    
 }
